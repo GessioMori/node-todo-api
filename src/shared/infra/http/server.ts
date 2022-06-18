8c7b97d9f9909709b1fa907d6846af708f9f5a56
Gessio Mori Neto <gessiomorin@gmail.com>
2022-06-17 21:57:34 -0300

    feat: creates postgres db

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,14 @@
 import { app } from "@shared/infra/http/app";
+import "reflect-metadata";
+import { AppDataSource } from "../typeorm";
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
