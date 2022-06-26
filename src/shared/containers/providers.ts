054233a7314bf374c9e1018ca744f8b22942d0a5
Gessio Mori Neto <gessiomorin@gmail.com>
2022-06-26 13:28:26 -0300

    feat: implements mail provider

--- src/shared/containers/providers.ts
+++ src/shared/containers/providers.ts
@@ -1,5 +1,12 @@
 import { IDateProvider } from "@shared/providers/dateProvider/IDateProvider";
 import { DayJsDateProvider } from "@shared/providers/dateProvider/implementations/DayJsDateProvider";
+import { IEmailProvider } from "@shared/providers/emailProvider/IEmailProvider";
+import { EtherealMailProvider } from "@shared/providers/emailProvider/implementations/EtherealMailProvider";
 import { container } from "tsyringe";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJsDateProvider);
+
+container.registerInstance<IEmailProvider>(
+  "EmailProvider",
+  new EtherealMailProvider()
+);
