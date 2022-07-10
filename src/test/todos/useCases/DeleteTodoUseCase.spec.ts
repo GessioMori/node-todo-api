406106a9d1e710337de333afa765f6a0c48ad2cc
Gessio Mori Neto <gessiomorin@gmail.com>
2022-07-10 19:56:59 -0300

    build: bug correct for building

--- src/test/todos/useCases/DeleteTodoUseCase.spec.ts
+++ src/test/todos/useCases/DeleteTodoUseCase.spec.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 
 import { ICreateTodoDTO } from "@modules/todos/dtos/ICreateTodoDTO";
-import { ITodo } from "@modules/todos/entities/ITodo";
 import { ITodosRepository } from "@modules/todos/repositories/ITodosRepository";
 import { CreateTodoUseCase } from "@modules/todos/useCases/createTodo/CreateTodoUseCase";
 import { DeleteTodoUseCase } from "@modules/todos/useCases/deleteTodo/DeleteTodoUseCase";
 import { AppError } from "@shared/errors/AppError";
 import { TodosRepositoryInMemory } from "@test/inMemoryRepositories/TodosRepositoryInMemory";
+import { mappedTodo } from "@utils/mappers/todoMapper";
 
 describe("Delete todo use case", () => {
   let todosRepository: ITodosRepository;
@@ -15,7 +15,7 @@ describe("Delete todo use case", () => {
 
   let todoData: ICreateTodoDTO;
 
-  let todo: ITodo;
+  let todo: mappedTodo;
 
   let deleteTodoUseCase: DeleteTodoUseCase;
 
