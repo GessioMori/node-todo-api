22af5d89fb7d4aabb1670d094661d5be2ace8dbd
Gessio Mori Neto <gessiomorin@gmail.com>
2022-07-13 23:10:12 -0300

    deploy: removes server from docker

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3333;
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized.");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
   })
   .catch((err) => {
     console.error("Error during data source initialization.", err);
